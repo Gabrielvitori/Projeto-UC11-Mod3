38937566eb202728f29e4556ff1e391e6d0e7c67
Gabrielvitori <160680510+Gabrielvitori@users.noreply.github.com>
2024-03-28 16:16:46 -0300

    Correção para salvar os dados no banco de dados

--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
@@ -1,11 +1,11 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
+import java.sql.SQLException;
+
 
 public class ProdutosDAO {
 
@@ -14,28 +14,38 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
 
-    public void cadastrarProduto(ProdutosDTO produto, Connection conexao) {
+    public void cadastrarProduto(ProdutosDTO produto) {
         
-        conn = new conectaDAO().connectDB();
+    try{
+         conn = new conectaDAO().connectDB();
+         
+         String sql = "INSERT INTO tabela_produtos (id, nome, valor, status) VALUES (?, ?, ?, ?)";
+         PreparedStatement pstmt = conn.prepareStatement(sql);
+         pstmt.setInt(1, produto.getId()); 
+         pstmt.setString(2, produto.getNome());
+         pstmt.setDouble(3, produto.getValor()); 
+         pstmt.setString(4, produto.getStatus()); 
+         
+         pstmt.executeUpdate();
+         pstmt.close();
+         conn.close();
+            
+         System.out.println("Produto cadastrado com sucesso.");
+         
+    }catch (SQLException e) {
         
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conexao = DriverManager.getConnection("jdbc:mysql://localhost/atividade1", "root", "Lolgabriel725@");
-            System.out.println("SUCESSO DE CONEXÃO");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Falha ao carregar a classe de conexão. Classe não encontrada");
-        } catch (SQLException e) {
-            System.out.println("Falha ao conectar com o banco. Erro de SQL");
-        }
-    }
+         System.out.println("Erro ao cadastrar produto: " + e.getMessage());
+         
+        } 
+    
+    } 
 
     public ArrayList<ProdutosDTO> listarProdutos() {
 
         return listagem;
     }
 
-    void cadastrarProduto(ProdutosDTO produto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    
+
 
 }
